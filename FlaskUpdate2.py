34fbfca2f02845fc7318e4cf78ad4964dfe6d2ec
kristhenmvs <53184840+kristhenmvs@users.noreply.github.com>
2024-09-10 18:36:57 -0700

    FlaskUpdate2.py

--- FlaskUpdate2.py
+++ FlaskUpdate2.py
@@ -15,11 +15,20 @@ Created on Mon Sep  9 17:08:58 2024
 from flask import Flask, request, render_template, redirect, session
 import sqlite3
 from flask_session import Session
+import webbrowser
+import threading
+import time
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
+
+
+def run_flask_app():
+    app.run(debug=True, use_reloader=False)
+    
 
 # Function to connect to the SQLite database
 def connect_db():
@@ -124,4 +133,14 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+    flask_thread = threading.Thread(target=run_flask_app)
+    flask_thread.start()
+    
+    # Add a short delay to ensure the server is up
+    time.sleep(2)
+    
+    # Path to the Chrome executable
+    chrome_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe %s'
+    
+    # Open the local webpage in Chrome
+    webbrowser.get(chrome_path).open_new_tab('http://127.0.0.1:5000/login')
